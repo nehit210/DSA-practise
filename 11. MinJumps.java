a74f47f71885ca31b8ff9376cfbcf7af9a9912b2
Nehit Benara <210nehit@gmail.com>
2021-01-07 19:14:16 +0530

    Update 11. MinJumps.java

--- 11. MinJumps.java
+++ 11. MinJumps.java
@@ -13,7 +13,11 @@ class Main{
      
         System.out.println("The mminimum number of jump to reach end is" + minJumps(arr));
     }    
-    static int minJumps(int[] arr){
+     static int minJumps(int[] arr){
+        if(arr[0]==0)
+        {
+            return -1;
+        }
         int jump[]=new int[arr.length];
         for(int i=0;i<arr.length;i++)
         {
@@ -22,16 +26,24 @@ class Main{
         jump[0]=0;
         for(int i=1;i<arr.length;i++)
         {
+            int c=0;
             for(int j=0;j<i;j++)
             {
                 if(i<=arr[j]+j&&jump[j]!=Integer.MAX_VALUE)
                 {
-                    jump[i]=Math.min(jump[i],jump[i+1]);
+                    c++;
+                    if(jump[i]>(jump[j]+1))
+                    {
+                        jump[i]=jump[j]+1;
+                    }
                 }
+            }
+            if(c==0)
+            {
+                return -1;
             }
         }
         return jump[arr.length-1];
-        // your code here
         }
     
 }
