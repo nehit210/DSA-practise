61b823f982b5d01cf513390d4fbe8befe5b31f6b
Nehit Benara <210nehit@gmail.com>
2021-01-10 23:49:56 +0530

    Merge the overlapping intervals

--- 13. Merge interval.java
+++ 13. Merge interval.java
@@ -2,18 +2,18 @@ class Solution
 {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> merged = new LinkedList<>();
+        LinkedList<int[]> arr = new LinkedList<>();
         for (int[] interval : intervals) 
         {
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) 
+            if (arr.isEmpty() || arr.getLast()[1] < interval[0]) 
             {
-                merged.add(interval);
+                arr.add(interval);
             }
             else
             {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                arr.getLast()[1] = Math.max(arr.getLast()[1], interval[1]);
             }
         }
-        return merged.toArray(new int[merged.size()][]);
+        return arr.toArray(new int[arr.size()][]);
     }
 }
